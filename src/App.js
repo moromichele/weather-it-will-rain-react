90905ac9c7d73377d41bb59579d085f39d56115e
moromichele <moro.michele1997@gmail.com>
2022-09-05 22:05:26 +0200

    cleaned input display, deleted unused code

--- src/App.js
+++ src/App.js
@@ -1,99 +1,93 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { getWeatherUrl } from "./util/myFunctions.js";
+import { getWeatherUrl, toLocaleString } from "./util/myFunctions.js";
 import Location from "./components/Location.js";
 import Prediction from "./components/Prediction.js";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [place, setPlace] = useState("Rome, IT");
-  const [coords, setCoords] = useState({ lat: "41.893", lng: "12.483" });
-  const [rainToday, setRainTyoday] = useState(-1);
-  const [rainTomorrow, setRainTomorrow] = useState(-1);
-  const [editMode, setEditMode] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+	const [data, setData] = useState(null);
+	const [place, setPlace] = useState("Rome, IT");
+	const [displayPlace, setDisplayPlace] = useState("Rome, IT");
+	const [rainToday, setRainTyoday] = useState(-1);
+	const [rainTomorrow, setRainTomorrow] = useState(-1);
+	const [editMode, setEditMode] = useState(false);
+	const [inputValue, setInputValue] = useState("");
 
-  const mapQuestKey = "cb0WGUj0cN4jOJDJDArGH75PkNrGxyFu";
+	const mapQuestKey = "cb0WGUj0cN4jOJDJDArGH75PkNrGxyFu";
 
-  useEffect(() => {
-    console.log("updating stuff");
-    const coordUrl = `https://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=${place}`;
-    axios({
-      url: coordUrl,
-      method: "GET",
-    })
-      .then((res) => {
-        console.log(res);
-        console.log("in coords metto", res.data.results[0].locations[0].latLng);
-        setCoords(res.data.results[0].locations[0].latLng);
-        return res.data.results[0].locations[0].latLng;
-      })
-      .then((thisCoords) => {
-        const weatherUrl = getWeatherUrl(thisCoords.lat, thisCoords.lng);
-        axios({
-          url: weatherUrl,
-          method: "GET",
-        })
-          .then((res) => {
-            console.log(res.data);
-            setData(res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [place]);
+	useEffect(() => {
+		const coordUrl = `https://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=${place}`;
+		axios({
+			url: coordUrl,
+			method: "GET",
+		})
+			.then((res) => {
+				setDisplayPlace(toLocaleString(res.data.results[0].locations[0]));
+				//console.log(res.data.results[0].locations[0]);
+				return res.data.results[0].locations[0].latLng;
+			})
+			.then((thisCoords) => {
+				const weatherUrl = getWeatherUrl(thisCoords.lat, thisCoords.lng);
+				axios({
+					url: weatherUrl,
+					method: "GET",
+				})
+					.then((res) => {
+						//console.log(res.data);
+						setData(res.data);
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [place]);
 
-  useEffect(() => {
-    console.log("datadatadata");
-    if (data) {
-      console.log("posto: " + place + ", " + coords.lat + " " + coords.lng);
-      console.log("pioggia oggi: " + data.daily.precipitation_hours[0]);
-      console.log("pioggia domani: " + data.daily.precipitation_hours[1]);
-      setRainTyoday(data.daily.precipitation_hours[0]);
-      setRainTomorrow(data.daily.precipitation_hours[1]);
-    }
-  }, [data]);
+	useEffect(() => {
+		if (data) {
+			setRainTyoday(data.daily.precipitation_hours[0]);
+			setRainTomorrow(data.daily.precipitation_hours[1]);
+		}
+	}, [data]);
 
-  const handleBodyClick = (e) => {
-    if (editMode && e.target.className !== "active-form") {
-      submitMyForm();
-    }
-  };
+	const handleBodyClick = (e) => {
+		if (editMode && e.target.className !== "active-form") {
+			submitMyForm();
+		}
+	};
 
-  const submitMyForm = () => {
-    setEditMode(false);
-    setPlace(inputValue.trim());
-    // TODO: clean input better
-  };
+	const submitMyForm = () => {
+		setEditMode(false);
+		setPlace(inputValue.trim());
+		// TODO: clean input better
+	};
 
-  return (
-    <div className="App" onClick={(e) => handleBodyClick(e)}>
-      <div className="app-content">
-        <header>
-          <h1>Rain?</h1>
-        </header>
-        <Location
-          place={place}
-          setPlace={setPlace}
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setInputValue={setInputValue}
-          submitMyForm={submitMyForm}
-        />
-        <div className="pred-container">
-          <div className="spacer"></div>
-          <Prediction date="Today" rainForDate={rainToday} />
-          <div className="spacer"></div>
-          <Prediction date="Tomorrow" rainForDate={rainTomorrow} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App" onClick={(e) => handleBodyClick(e)}>
+			<div className="app-content">
+				<header>
+					<h1>Rain?</h1>
+				</header>
+				<Location
+					place={displayPlace}
+					setPlace={setPlace}
+					editMode={editMode}
+					setEditMode={setEditMode}
+					setInputValue={setInputValue}
+					submitMyForm={submitMyForm}
+				/>
+				<div className="pred-container">
+					<div className="spacer"></div>
+					<Prediction date="Today" rainForDate={rainToday} />
+					<div className="spacer"></div>
+					<Prediction date="Tomorrow" rainForDate={rainTomorrow} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/util/myFunctions.js
+++ src/util/myFunctions.js
@@ -1,34 +1,42 @@
-const getCoords = (city) => {};
-
-const getWeather = (coords) => {};
-
 const rainToString = (rainData) => {
-  // TODO: better logic.....
-  if (rainData === 0) return "No";
-  if (rainData >= 3) return "Yes";
-  if (rainData === 2) return "Yeah";
-  return "Maybe";
+	// TODO: better logic.....
+	if (rainData === 0) return "No";
+	if (rainData >= 3) return "Yes";
+	if (rainData === 2) return "Yeah";
+	return "Maybe";
 };
 
 const zeroPad = (num, places) => String(num).padStart(places, "0");
 
 const formatDate = (dateObj) => {
-  var day = zeroPad(dateObj.getDate(), 2);
-  var month = zeroPad(dateObj.getMonth() + 1, 2);
-  var year = dateObj.getFullYear();
-  return "" + year + "-" + month + "-" + day;
+	var day = zeroPad(dateObj.getDate(), 2);
+	var month = zeroPad(dateObj.getMonth() + 1, 2);
+	var year = dateObj.getFullYear();
+	return "" + year + "-" + month + "-" + day;
 };
 
 const getWeatherUrl = (lat, lng) => {
-  const myTimeZone = encodeURIComponent(
-    Intl.DateTimeFormat().resolvedOptions().timeZone
-  );
-  const today = formatDate(new Date());
-  const tomorrow = formatDate(
-    new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
-  );
-  console.log(today, tomorrow);
-  return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours&timezone=${myTimeZone}&start_date=${today}&end_date=${tomorrow}`;
+	const myTimeZone = encodeURIComponent(
+		Intl.DateTimeFormat().resolvedOptions().timeZone
+	);
+	const today = formatDate(new Date());
+	const tomorrow = formatDate(
+		new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+	);
+	return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours&timezone=${myTimeZone}&start_date=${today}&end_date=${tomorrow}`;
 };
 
-export { getCoords, getWeather, rainToString, getWeatherUrl };
+const toLocaleString = (l) => {
+	let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+
+	if (!l) return "Error: bad location";
+	if (l.adminArea5 === "") {
+		if (l.adminArea3 === "") {
+			return regionNames.of(l.adminArea1);
+		}
+		return l.adminArea3 + ", " + l.adminArea1;
+	}
+	return l.adminArea5 + ", " + l.adminArea1;
+};
+
+export { rainToString, getWeatherUrl, toLocaleString };
